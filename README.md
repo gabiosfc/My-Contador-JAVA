4d28774d29b2ecf30e444be004a0968a544adbe6
Gabriela <gabiosfc@gmail.com>
2024-11-03 19:13:10 -0300

    Ajustes de layuot

---
+++ README.md
@@ -0,0 +1,25 @@
+<h1 align="center">Contador</h1>
+
+## Project
+This project consists of an Android application that implements a simple counter. The application allows users to increment and decrement a counter through two buttons. It features a clean and intuitive user interface with buttons styled in blue and elements centered vertically on the screen.
+
+## Layout
+<div align="center">
+  <img src="./images/contador_java.jpeg" alt="App Screenshot" width="400"/>
+</div>
+
+## Features
+- Increment and decrement buttons to adjust the counter.
+- The counter value is displayed in the center of the screen.
+- Buttons are styled with a blue background.
+
+User-friendly and responsive design.
+## Technologies Used
+- **Programming Language:** Java
+- **Development Environment:** Android Studio
+
+## Code Author
+```kotlin
+fun main() {
+    println("Code by Gabriela Oliveira!")
+}
--- app/src/main/java/com/example/mycontadorjava/MainActivity.java
+++ app/src/main/java/com/example/mycontadorjava/MainActivity.java
@@ -4,11 +4,7 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,18 +13,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
 
         // Referências aos elementos do layout
         TextView textContador = findViewById(R.id.textView);
         Button btnAumentar = findViewById(R.id.activity_main_btn_aumentar);
-        Button btnDiminuir = findViewById(R.id.ativity_main_btn_diminuir);
+        Button btnDiminuir = findViewById(R.id.activity_main_btn_diminuir);
 
         // Inicializa o valor do contador na TextView
         textContador.setText(String.valueOf(contador));
@@ -55,4 +45,3 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 }
-
